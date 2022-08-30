247f89121b12b3182ac4d03025710840d57984fb
Gabriel Kleiman <kleim4n@gmail.com>
2022-08-29 23:48:25 -0300

    Capable to move from deck (black) to unused (red).

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const deck_id = 'zz45tbc98nyy';
 
 class Card{
     constructor(code, image){
@@ -25,12 +24,31 @@ class Game{
     resetDeck(){
         $.getJSON(`https://www.deckofcardsapi.com/api/deck/${this.deck_id}/return/`);
     }
+    resetGame(){
+        $.getJSON('https://www.deckofcardsapi.com/api/deck/new/', function(json){
+            this.deck_id = json.deck_id;
+            localStorage.setItem('deck_id', json.deck_id);
+            console.log(game.deck_id);
+            //need to delete all cards before
+            game.generateIMGsOnHTML();
+        });
+    }
     generateIMGsOnHTML(){
         //Get all the 52 cards
         $.getJSON(`https://www.deckofcardsapi.com/api/deck/${this.deck_id}/draw/?count=52`, function(json){
             json.cards.forEach(function(card){
                 //Put each card in it respective div by suit
-                $('.divDeck'+card.suit).append(`<img id="${card.code}" src="${card.image}" alt="${card.code}">`);
+                $('.divDeck'+card.suit).append(`<img id="d${card.code}" src="${card.image}" alt="${card.code}">`);
+                $('.divUnused').append(`<img id="u${card.code}" src="${card.image}" alt="${card.code}">`);
+                $(`#u${card.code}`).hide();
+                $(`#d${card.code}`).click(function(){
+                    $(`#d${card.code}`).hide();
+                    $(`#u${card.code}`).show();
+                });
+                $(`#u${card.code}`).click(function(){
+                    $(`#u${card.code}`).hide();
+                    $(`#d${card.code}`).show();
+                });
             });
             //$.getJSON(`https://www.deckofcardsapi.com/api/deck/${this.deck_id}/pile/unused/add/?cards=${card.cards[0].code}`);
         });
@@ -38,11 +56,14 @@ class Game{
 }
 
 $(document).ready(function(){
-    $.getJSON('https://www.deckofcardsapi.com/api/deck/new/', function(deck){
+    var urlEnd = (localStorage.getItem('deck_id') === null)?('new/'):(localStorage.getItem('deck_id')+'/');
+    $.getJSON(`https://www.deckofcardsapi.com/api/deck/${urlEnd}`, function(deck){
+        if (urlEnd == 'new/') localStorage.setItem('deck_id', deck.deck_id);
         var game = new Game(deck.deck_id);
         console.log(game.deck_id);
         $('.buttonNewCard').click(function(){game.getNewCard();});
         $('.buttonResetDeck').click(function(){game.resetDeck();});
+        $('.buttonResetGame').click(function(){game.resetGame();});
         game.generateIMGsOnHTML();
     });
 });
